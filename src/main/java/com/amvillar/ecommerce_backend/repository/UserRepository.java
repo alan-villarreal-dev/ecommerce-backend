834aa8e0a635d300a24addb1b1bad689c06a22aa
Alan Villarreal <nydhoggr@protonmail.com>
2026-02-02 18:37:12 -0600

    feat: Method to verify if mail already exists in the database

--- src/main/java/com/amvillar/ecommerce_backend/repository/UserRepository.java
+++ src/main/java/com/amvillar/ecommerce_backend/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
     // Extending JpaRepository we have automatically methods like:
     // .save() -> Save
     // .findAll() -> Find All
