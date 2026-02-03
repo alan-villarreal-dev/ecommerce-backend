54d13e4175b4263ed5fe341b251b933d547be39a
Alan Villarreal <nydhoggr@protonmail.com>
2026-02-02 19:03:08 -0600

    fix: RestController and mapping added

--- src/main/java/com/amvillar/ecommerce_backend/controller/UserController.java
+++ src/main/java/com/amvillar/ecommerce_backend/controller/UserController.java
@@ -6,7 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.amvillar.ecommerce_backend.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/auth")
 public class UserController {
     private final UserService userService;
 
