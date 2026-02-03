c28ec771a2aba3def578e2988498641673f89922
Alan Villarreal <nydhoggr@protonmail.com>
2026-02-02 18:50:02 -0600

    feat: implemented security rules for user registration

--- src/main/java/com/amvillar/ecommerce_backend/config/SecurityConfig.java
+++ src/main/java/com/amvillar/ecommerce_backend/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.amvillar.ecommerce_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +14,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                // 1. Disable CSRFD
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // 2. Setup route permissions
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // Allow register/login
+                        .anyRequest().authenticated()            // Everything else requires a password
+                );
+
+        return http.build();
     }
 }
